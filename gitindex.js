857022ba6f66668a3c6858e9f029a8d7d207bbea
리얼리즘산타 <jinhojun00@gmail.com>
2021-10-02 13:45:04 +0000

    push to commit

--- gitindex.js
+++ gitindex.js
@@ -1,3 +1,6 @@
-function source(args){
-	console.log("TEST");
-}
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send("It was changed"));
+app.listen(port, ()=> console.log("HI ${port}"));
