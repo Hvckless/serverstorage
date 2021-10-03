1b50acea7c4eedc64da810fcaf7d5781a4b2fa4c
리얼리즘산타 <jinhojun00@gmail.com>
2021-10-03 07:04:35 +0000

    UTF-8 setup

--- gitindex.js
+++ gitindex.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = http.createServer(function(req, res){
 	var url = req.url;
-	res.writeHead(200);
+	res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
 	res.write(req.url + "쿼리된 URL");
 	res.end();
 });
