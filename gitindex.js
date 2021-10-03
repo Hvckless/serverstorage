5221861feb611311eca6111efad164f6d3dd05b0
리얼리즘산타 <jinhojun00@gmail.com>
2021-10-03 07:12:52 +0000

    port checker

--- gitindex.js
+++ gitindex.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var app = http.createServer(function(req, res){
 	var url = req.url;
 	res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-	res.write(req.url + "쿼리된 URL");
+	res.write(req.url + "쿼리된 URL" + process.env.PORT || 3000);
 	res.end();
+	//if()
 });
 
 app.listen(process.env.PORT || 3000);
